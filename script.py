7c6c722fb519d4af3b4b13b55087a1316902bd80
Esteban Becker <git@beckeresteban.eu>
2022-04-04 10:41:08 +0200

    Change flipping coin algorithm

--- script.py
+++ script.py
@@ -5,23 +5,24 @@ import numpy as np
 
 random.seed(time.time())
 
-def unfair_coin_flip(n, p):
-    coin = []
-    for i in range(n):
-        if random.random() < p:
-            coin.append(0)
-        else:
-            coin.append(1)
-    return coin
+def unfair_coin_flip(p):
+    coin_list = []
+    for i in range(30):
+        coin=0
+        for j in range(50):
+            if random.random() < p:
+                coin += 1
+        coin_list.append(coin)
+    return coin_list
 
 #Flip 30 times a coin
-coin1 = [random.randint(0,1) for i in range(30)]
+coin1 = unfair_coin_flip(p=0.5)
 
 #Flip 30 times an unfair coins (25/75),(10/90),(45/55),(52/48)
-coin2 = unfair_coin_flip(30, p=0.75)
-coin3 = unfair_coin_flip(30, p=0.9)
-coin4 = unfair_coin_flip(30, p=0.55)
-coin5 = unfair_coin_flip(30, p=0.48)
+coin2 = unfair_coin_flip(p=0.75)
+coin3 = unfair_coin_flip(p=0.9)
+coin4 = unfair_coin_flip(p=0.55)
+coin5 = unfair_coin_flip(p=0.48)
 
 fig1 = plt.figure()
 fig1.suptitle('Fair Coin Flip')
